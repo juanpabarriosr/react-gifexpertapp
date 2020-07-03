5a8c66f05cd832063762e9eb9f9b047170ed2bfb
juanpabarriosr <juanpabarriosr@gmail.com>
2020-07-03 01:17:48 -0500

    useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const GifGrid = ({ category }) => {
+
+    const [count, setCount] = useState(0);
+
+    useEffect( () => {
+        getGifs();
+    }, [] )
 
     const getGifs = async() => {
 
@@ -21,11 +27,13 @@ export const GifGrid = ({ category }) => {
 
     }
 
-    getGifs();
+    // getGifs();
 
     return (
         <div>
             <h3> { category } </h3>
+            <h3> { count } </h3>
+            <button onClick={ () => setCount( count + 1 ) } > + 1 </button>
         </div>
     )
 }
