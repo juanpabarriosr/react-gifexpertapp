bc8ebd9030f20a6dc0dd82c5ab92bfeb2b228374
juanpabarriosr <juanpabarriosr@gmail.com>
2020-07-06 01:57:11 -0500

    Helpers - getGifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,33 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import { GifGridItem } from './GifGridItem';
+import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
 
     const [images, setImages] = useState([]);
 
     useEffect( () => {
-        getGifs();
-    }, [] )
+        getGifs( category )
+            .then( setImages );
+    }, [ category ] )
 
-    const getGifs = async() => {
-
-        const url = 'https://api.giphy.com/v1/gifs/search?q=Cats&limit=10&api_key=Fa4KtfsYDgRjtL9ccgNflrzbyr8lNdAH';
-
-        const resp = await fetch( url );
-        const { data } = await resp.json();
-
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-
-        console.log(gifs);
-        setImages( gifs );
-
-    }
+    
 
     return (
         <>
