acd7664c2bebf2ddc9199f6580be17583060e8b4
juanpabarriosr <juanpabarriosr@gmail.com>
2020-07-08 00:46:53 -0500

    docs agregado

---
+++ docs/precache-manifest.3bdb21e6fec8ad67532d4d255c00e5a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa7cbb12ba433ebdabf5bba3c349ee5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f40c5b855c05432a6aa6",
+    "url": "/static/css/main.6c596a41.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f40c5b855c05432a6aa6",
+    "url": "/static/js/main.424aa429.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
